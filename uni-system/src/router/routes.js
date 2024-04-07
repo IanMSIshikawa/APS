91755461adaa8b79195b7f5b99e3e0c90afdb560
IanMSIshikawa <ianishikawa1@gmail.com>
2024-04-07 19:51:03 -0300

    alterando routes.js

--- uni-system/src/router/routes.js
+++ uni-system/src/router/routes.js
@@ -1,10 +1,12 @@
 const routes = [
   {
+    // path: '/',
+    // component: () => import('layouts/MainLayout.vue'),
+    // children: [
+    //   { path: '', component: () => import('pages/Index.vue') }
+    // ]
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+    component: () => import('pages/HelloWorld.vue')
   },
 
   // Always leave this as last one,
