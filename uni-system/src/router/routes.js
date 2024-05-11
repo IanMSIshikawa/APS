c0b4091418efde084dd32bd70c0d2ae6c0afbe1e
IanMSIshikawa <ianishikawa1@gmail.com>
2024-05-11 18:34:59 -0300

    tela de informações pronta. TODO: terminar textos

--- uni-system/src/router/routes.js
+++ uni-system/src/router/routes.js
@@ -1,14 +1,16 @@
 const routes = [
   {
-    // path: '/',
-    // component: () => import('layouts/MainLayout.vue'),
-    // children: [
-    //   { path: '', component: () => import('pages/Index.vue') }
-    // ]
     path: '/',
-    component: () => import('pages/paginaLogin.vue')
+    component: () => import('pages/paginaGuia.vue')
   },
-
+  {
+    path: '/layout',
+    component: () => import('../layouts/MainLayout.vue')
+  },
+  {
+    path: '/login',
+    component: () => import('../pages/paginaLogin.vue')
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
